2eeb6056f55925aadbaa8065157190e4e13d48c0
Oshita Shiko <zangcoder0210@gmail.com>
2021-09-23 19:29:42 +0300

    pool ui finished

---
+++ Frontend-UI-Pool/src/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.less";
+import GitHubButton from "react-github-btn";
+import { Routes } from "./routes";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <div className="Banner">
+        <div
+          className="Banner-description"
+          style={{
+            textAlign: 'left',
+            paddingLeft: '30px',
+            paddingTop: '5px'
+          }}
+        >
+        </div>
+      </div> */}
+      <Routes />
+{/*       
+      <div className="social-buttons">
+        <GitHubButton
+          href="https://github.com/project-serum/oyster-swap"
+          data-color-scheme="no-preference: light; light: light; dark: light;"
+          data-icon="octicon-star"
+          data-size="large"
+          data-show-count={true}
+          aria-label="Star solana-labs/oyster-swap on GitHub"
+        >
+          Star
+        </GitHubButton>
+        <GitHubButton
+          href="https://github.com/project-serum/oyster-swap/fork"
+          data-color-scheme="no-preference: light; light: light; dark: light;"
+          data-size="large"
+          aria-label="Fork project-serum/oyster-swap on GitHub"
+        >
+          Fork
+        </GitHubButton>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
